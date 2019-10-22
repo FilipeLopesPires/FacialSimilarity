9102e6db50c885501ec91c332d320d17a2082768
Filipe Pires <filipesnetopires@ua.pt>
2019-10-22 15:30:29 +0100

    Proj2: example histogram is now plotted with gnuplot.

---
+++ proj2/src-example/build/CMakeFiles/wavcp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/fp/Desktop/4A1S/TAI/P/TAI/proj2/bin-example/wavcp"
+  "/home/fp/Desktop/4A1S/TAI/P/TAI/proj2/bin-example/wavcp.pdb"
+  "CMakeFiles/wavcp.dir/wavcp.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wavcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
