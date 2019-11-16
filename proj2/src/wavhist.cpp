526785d433aba19d8a25590333d0315b7bf269b8
Filipe Pires <filipesnetopires@ua.pt>
2019-11-16 17:26:38 +0000

    wavquant improvement; report continuation.

--- proj2/src/wavhist.cpp
+++ proj2/src/wavhist.cpp
@@ -1,58 +1,54 @@
-#include "wavhist.h"
 #include <iostream>
-#include <sndfile.hh>
+#include <fstream>
 #include <vector>
+#include <sndfile.hh>
+
+#include "headers/wavhist.h"
 
 using namespace std;
 
-constexpr size_t FRAMES_BUFFER_SIZE = 65536;  // Buffer for reading frames
+constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        cerr << "Usage: wavhist <input file> <channel>" << endl;
-        return 1;
+
+	if(argc < 3) {
+		cerr << "Usage: wavhist inputFile channel" << endl;
+		return 1;
+	}
+
+	SndfileHandle sndFile { argv[argc-2] };
+	if(sndFile.error()) {
+		cerr << "Error: invalid input file" << endl;
+		return 1;
     }
 
-    SndfileHandle sndFile{argv[argc - 2]};
-    if (sndFile.error()) {
-        cerr << "Error: invalid input file" << endl;
-        return 1;
-    }
+	if((sndFile.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV) {
+		cerr << "Error: file is not in WAV format" << endl;
+		return 1;
+	}
 
-    if ((sndFile.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV) {
-        cerr << "Error: file is not in WAV format" << endl;
-        return 1;
-    }
+	if((sndFile.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
+		cerr << "Error: file is not in PCM_16 format" << endl;
+		return 1;
+	}
 
-    if ((sndFile.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
-        cerr << "Error: file is not in PCM_16 format" << endl;
-        return 1;
-    }
+	int channel { stoi(argv[argc-1]) };
+	if(channel >= sndFile.channels()) {
+		cerr << "Error: invalid channel requested" << endl;
+		return 1;
+	}
 
-    int channel{stoi(argv[argc - 1])};
-    if (channel >= sndFile.channels()) {
-        cerr << "Error: invalid channel requested" << endl;
-        return 1;
-    }
+	size_t nFrames;
+	vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
+	WAVHist hist { sndFile };
+	while((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
+		samples.resize(nFrames * sndFile.channels());
+		hist.update(samples);
+	}
+	
+	hist.dump(channel);
+	hist.fwrite(channel,"histogram.txt");
+	system("gnuplot -p -e \"plot 'histogram.txt'\"");
 
-    // size_t nFrames;
-    // vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
-    // WAVHist hist{sndFile};
-    // while ((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
-    //     samples.resize(nFrames * sndFile.channels());
-    //     hist.update(samples);
-    // }
-
-    size_t nFrames;
-    size_t totalFrame;
-    vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
-    WAVHist hist{sndFile};
-    while ((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
-        totalFrame += nFrames;
-        samples.resize(nFrames * sndFile.channels());
-        hist.update(samples);
-    }
-
-    hist.dump(channel);
-    return 0;
+	return 0;
 }
--- proj2/src/wavquant.cpp
+++ proj2/src/wavquant.cpp
@@ -11,30 +11,28 @@ using namespace std;
 constexpr size_t FRAMES_BUFFER_SIZE =
     65536;  // Buffer for reading/writing frames
 
-constexpr int REDUCTFACTOR = 4;
+// default values for the reduction factor and the number of quantization bits
+int REDUCTFACTOR = 1;
+int QUANTSIZE = 16;
 
-void parseArguments(int argc, char* argv[],
-                    SndfileHandle& sndFileIn, SndfileHandle& sndFileOut, int& numQuantBits);
+SndfileHandle parseArguments(int argc, char* argv[], SndfileHandle& sndFileIn);
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        cerr << "Usage: wavquant <quant size> <input file> <output file>"
+    if (argc < 3 || (argc != 5 && argc != 7)) {
+        cerr << "Usage: wavquant inputFile outputFile [-q quantSize] [-r reductFactor]"
              << endl;
         return 1;
     }
 
     // parse and validate arguments
-    SndfileHandle sndFileIn{argv[argc - 2]};
-    SndfileHandle sndFileOut{argv[argc - 1], SFM_WRITE, sndFileIn.format(), 1,
-                             sndFileIn.samplerate() / 4};
-    int numQuantBits;
-    parseArguments(argc, argv, sndFileIn, sndFileOut, numQuantBits);
+    SndfileHandle sndFileIn{argv[1]};
+    SndfileHandle sndFileOut = parseArguments(argc, argv, sndFileIn);
 
+    // conversion and uniform scalar quantization
     size_t nFrames;
     size_t n{};
     short tmpFreq{};
     vector<short> sampleReduct;
-
     vector<short> samples(FRAMES_BUFFER_SIZE * sndFileIn.channels());
     vector<short> mySamples;
     while ((nFrames = sndFileIn.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
@@ -42,7 +40,7 @@ int main(int argc, char *argv[]) {
             tmpFreq += s;
             if (++n % sndFileIn.channels() == 0) {
                 tmpFreq /= sndFileIn.channels();
-                tmpFreq = (tmpFreq & (-1 << sizeof(short) * 8 - numQuantBits));
+                tmpFreq = (tmpFreq & (-1 << sizeof(short) * 8 - QUANTSIZE));
                 sampleReduct.push_back(tmpFreq);
                 if (sampleReduct.size() >= REDUCTFACTOR) {
                     mySamples.push_back(
@@ -60,17 +58,65 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void parseArguments(int argc, char* argv[],
-                    SndfileHandle& sndFileIn, SndfileHandle& sndFileOut, int& numQuantBits) {
+SndfileHandle parseArguments(int argc, char* argv[], SndfileHandle& sndFileIn) {
 
-    checkFileToRead(sndFileIn, argv[argc - 2]);
+    // this could be optimized...
+    if (argc > 3) {
+        string option = argv[3];
+        if (option != "-q" && option != "-r") {
+            cerr << "Error: invalid option definition: must be either '-q' or '-r'" << endl;
+            exit(1);
+        }
 
-    checkFileOpenSuccess(sndFileOut, argv[argc - 1]);
+        if (option == "-q") {
+            int quantSize = stoi(argv[4]);
+            if (quantSize <= 0 || quantSize > 16) {
+                cerr << "Error: invalid quantSize: must be between 1 and 16" << endl;
+                exit(1);
+            }
+            QUANTSIZE = quantSize;
 
-    numQuantBits = stoi(argv[argc - 3]);
-    if (numQuantBits <= 0 || numQuantBits > sizeof(short) * 8) {
-        cerr << "Error: invalid quant. bits number, must be between 1 and 16"
-             << endl;
-        exit(1);
+            if (argc == 7) {
+                option = argv[5];
+                if (option != "-r") {
+                    cerr << "Error: invalid option definition: must be either '-q' or '-r' (and you cannot repeat them)" << endl;
+                    exit(1);
+                }
+                int reductFactor = stoi(argv[6]);
+                if (reductFactor <= 0 || reductFactor > 16) {
+                    cerr << "Error: invalid reductFactor: must be between 1 and 16" << endl;
+                    exit(1);
+                }
+                REDUCTFACTOR = reductFactor;
+            }
+        } else {
+            int reductFactor = stoi(argv[4]);
+            if (reductFactor <= 0 || reductFactor > 16) {
+                cerr << "Error: invalid reductFactor: must be between 1 and 16" << endl;
+                exit(1);
+            }
+            REDUCTFACTOR = reductFactor;
+
+            if (argc == 7) {
+                option = argv[5];
+                if (option != "-q") {
+                    cerr << "Error: invalid option definition: must be either '-q' or '-r' (and you cannot repeat them)" << endl;
+                    exit(1);
+                }
+                int quantSize = stoi(argv[6]);
+                if (quantSize <= 0 || quantSize > 16) {
+                    cerr << "Error: invalid quantSize: must be between 1 and 16" << endl;
+                    exit(1);
+                }
+                QUANTSIZE = quantSize;
+            }
+        }
     }
+
+    checkFileToRead(sndFileIn, argv[1]);
+
+    SndfileHandle sndFileOut{argv[2], SFM_WRITE, sndFileIn.format(), 1, sndFileIn.samplerate() / REDUCTFACTOR};
+    checkFileOpenSuccess(sndFileOut, argv[2]);
+
+    return sndFileOut;
 }
